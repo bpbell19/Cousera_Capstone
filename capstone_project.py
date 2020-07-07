6fa6808b25c750f3176931708f6ce093433fb3ad
Brandon Bell <bpbell19@gmail.com>
2020-07-07 17:38:48 -0500

    Welcome to Brandon's Capstone Project.

---
+++ capstone_project.py
@@ -0,0 +1,5 @@
+vim python_code.pi
+### This notebook will primarily be used for the Data Science Certificate course captone project.
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
